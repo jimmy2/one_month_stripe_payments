1fa5fe9f9e20cf5c533537b0b61c9c78d6a9d4a3
James Ferguson <jimmy2289@gmail.com>
2014-08-13 06:48:01 +0900

    Saving generated rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OneMonthStripePayments::Application.config.secret_key_base = 'a1d9bd65becbae0775503955e5632cdd71631a3417974bd13dab72cc10f985415ae40cc119bff20dff003bf2d0e3161664167ea1d06fafc4108b9cd459c1e5dd'
